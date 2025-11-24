2340a784b97d00345b8efcdeb40e3861ee4ffcf1
BillyGrace8 <bielgracia8@gmail.com>
2025-11-24 18:43:58 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+// Servir la carpeta src/ (imatges, CSS, etc.)
+app.use(express.static(path.join(__dirname, "src")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor en marxa al port ${PORT}`);
+  console.log(`Servidor funcionant al port ${PORT}`);
 });
+
+
